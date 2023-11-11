b6af265b4badea321465a26032faa9685f2a3b0c
ShivAnand Y <shivay4318@gmail.com>
2023-11-11 12:54:48 +0530

    separates pending files

---
+++ mircoservices/database.js
@@ -0,0 +1,30 @@
+const users = [
+    {
+        email:"john@gmail.com",
+        password:"random12"
+    },
+    {
+        email:"shiva@gmail.com",
+        password:"shivay123"
+    }
+]
+
+const publicPosts = [
+    {
+      title: "Post 1",
+      content: "Post 1 is free",
+    },
+    {
+      title: "Post 2",
+      content: "Post 2 is free",
+    },
+  ];
+  
+  const privatePosts = [
+    {
+      title: "Post 3",
+      content: "Post 3 is private",
+    },
+  ];
+  
+  module.exports = { users, publicPosts, privatePosts };
