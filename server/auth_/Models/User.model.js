d9551e412bb7658c0ec31ef84aa5e1c20694fdfd
ShivAnand Y <shivay4318@gmail.com>
2023-11-11 17:45:26 +0530

    implement login & registration

--- server/auth_/Models/User.model.js
+++ server/auth_/Models/User.model.js
@@ -1,42 +1,50 @@
-const mongoose =require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
-    firstName:{
-        type:String,
-        required:true,
-        
-    },
-    lastName:{
-        type:String,
-        required:false,
-    },
-    email:{
-        type:String,
-        required:true,
-        lowercase:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-})
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
 // middle ware to work before saving the data in db (eg: hashing password)
-UserSchema.pre('save',async function (next){
-    try{
-        // console.log("before saving the data");
-        const salt = await bcrypt.genSalt(10) 
-        // console.log(this.email, this.password);
-        const hashedPassword = await bcrypt.hash(this.password,salt);
-        this.password = hashedPassword;
-        next();
-    }catch(error){
-        console.log(error);
-    }
-})
+UserSchema.pre("save", async function (next) {
+  try {
+    // console.log("before saving the data");
+    const salt = await bcrypt.genSalt(10);
+    // console.log(this.email, this.password);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-const User = mongoose.model('user',UserSchema)
+// check login password is valid or not
+UserSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const User = mongoose.model("user", UserSchema);
 module.exports = User;
--- server/auth_/Routes/Auth_routes.js
+++ server/auth_/Routes/Auth_routes.js
@@ -1,45 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const createError = require("http-errors");
-const User = require("../Models/User.model");
-const { authSchema } = require("../helpers/validation_schema");
-const {signAccessToken} = require('../helpers/jwt_helper');
+const {
+  register,
+  login,
+  refreshToken,
+  logout,
+} = require("../Controllers/Auth.Controller");
+router.post("/register", register);
 
-router.post("/register", async (req, res, next) => {
-  console.log(req.body);
-  // res.send('registration ');
-  try {
-    // const {firstName,lastName,email,password,confirmPassword} = req.body;
-    // if(!email || !password){
-    //     throw createError.BadRequest();
-    // }
-    const validResult = await authSchema.validateAsync(req.body);
-    const doesExit = await User.findOne({ email: validResult.email });
-    if (doesExit)
-      throw createError.Conflict(
-        `${validResult.email} is already been registered`
-      );
+router.post("/login", login);
 
-    const user = new User(validResult);
-    const savedUser = await user.save();
-    const accessToken = await signAccessToken(savedUser.id);
-    res.send({accessToken});
-    // res.send(savedUser);
-  } catch (error) {
-    if (error.isJoi) error.status = 422;
-    next(error);
-  }
-});
-router.post("/login", async (req, res, next) => {
-  res.send("login route");
-});
+router.post("/refresh-token", refreshToken);
 
-router.post("/refresh-token", async (req, res, next) => {
-  res.send("refresh Token route");
-});
-
-router.delete("/logout", async (req, res, next) => {
-  res.send("logout route");
-});
+router.delete("/logout", logout);
 
 module.exports = router;
